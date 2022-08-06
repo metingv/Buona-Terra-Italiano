d0d0318c041b43ac79f08aa9e7748b91cf93a49a
metin <met.guliyev11@gmail.com>
2022-08-06 16:44:43 +0400

    menu page

--- src/Pages/Accolades.jsx
+++ src/Pages/Accolades.jsx
@@ -1,6 +1,77 @@
 import Navbar from "../Components/Navbar";
 import Footer from '../Components/Footer'
+import { useState } from 'react'
 function Accolades() {
+
+    const [imgarray] = useState([
+        {
+            src: "https://www.buonaterra.com.sg/wp-content/gallery/accolades/50topitaly2022.png",
+            title: "50 Top Italy ",
+            text: " 35° in 50 Best Italian Restaurants in The World"
+        },
+        {
+            src: "https://www.buonaterra.com.sg/wp-content/gallery/accolades/50BEST-DISCOVERY-MARK-BLACK-HIGH-RES.png",
+            title: "50 Best Discovery",
+            text: "Listed on 50 Best Discovery"
+        },
+        {
+            src: "https://www.buonaterra.com.sg/wp-content/gallery/accolades/MICHELIN-STAR-website.png",
+            title: "Singapore Michelin Guide",
+            text: "Award 1 Star, 2021"
+        },
+        {
+            src: "https://www.buonaterra.com.sg/wp-content/gallery/accolades/BT_Michelin2019.jpg",
+            title: "Singapore Michelin Guide",
+            text: " Debut 1 Star, 2019"
+        },
+        {
+            src: "https://www.buonaterra.com.sg/wp-content/gallery/accolades/accolade-img1.jpg",
+            title: "Michelin Guide Singapore",
+            text: " Listed on 2016 - 2022"
+        },
+        {
+            src: "https://www.buonaterra.com.sg/wp-content/gallery/accolades/accolade-img2.jpg",
+            title: "Top Italian Restaurants ",
+            text: " Three Forks 2017, 2019 - 2022"
+        },
+        {
+            src: "https://www.buonaterra.com.sg/wp-content/gallery/accolades/PEAK-GRA2018-262-X-256.jpg",
+            title: "The Peak G Restaurants Award",
+            text: "Award Of Excellence 2016 - 2018"
+        },
+        {
+            src: "https://www.buonaterra.com.sg/wp-content/gallery/accolades/T-DiningLogo_1819-262-x-256.jpg",
+            title: "Singapore Tatler T Dining Award",
+            text: "Singapore Best Restaurants 2014, 2017, 2019"
+        },
+        {
+            src: "https://www.buonaterra.com.sg/wp-content/gallery/accolades/T-DiningLogo_1819-262-x-256-1.jpg",
+            title: "Singapore Tatler T Dining Award",
+            text: "Regional Best Restaurants 2014"
+        },
+        {
+            src: "https://www.buonaterra.com.sg/wp-content/gallery/accolades/TRG-2017-262-x-256.jpg",
+            title: "Wine & Dine Singapore’s Top Restaurants ",
+            text: "One Star (2015), Two Stars (2016)"
+        },
+        {
+            src: "https://www.buonaterra.com.sg/wp-content/gallery/accolades/accolade-img6.jpg",
+            title: "RAS Epicurean Star Award",
+            text: "Best Of The Best Fine Dining 2016"
+        },
+        {
+            src: "https://www.buonaterra.com.sg/wp-content/gallery/accolades/thumbs_accolade-img6-262-X-256.jpg",
+            title: "RAS Epicurean Star Award",
+            text: "Best Of The Western Fine Dining 2016"
+        }
+    ])
+    const [load,setLoad ] = useState(8)
+
+    const OnLoadMore = () => {
+         setLoad(12)
+    }
+        
+    
     return (
         <>
 
@@ -8,6 +79,30 @@ function Accolades() {
                 <header>
                     <Navbar />
                 </header>
+                <section className="Globalcss GlobalComponent">
+                    <h1>Accolades</h1>
+                    <div className="items">
+                        {
+                            imgarray.slice(0,load).map((img) => {
+                                return (
+                                    <figure>
+                                        <img src={img.src} alt="buonaterra" />
+                                        <div className="text">
+                                            <div className="title">
+                                                {img.title}
+                                            </div>
+                                            <div>
+                                            {img.text}
+                                            </div>
+
+                                        </div>
+                                    </figure>
+                                )
+                            })
+                        }
+                    </div>
+                    <button onClick={OnLoadMore}>Load More</button>
+                </section>
             </div>
             <Footer />
         </>
--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -3,11 +3,79 @@ import Footer from '../Components/Footer'
 function Menu() {
     return (
         <>
-        <Navbar />
-        <div className="Menu">
-        
-        </div>
-        <Footer />
+            <div className="Menu">
+                <header>
+                    <Navbar />
+                </header>
+                <div className="GlobalComponent1 Component1a" >
+                    <figure>
+                        <h1>CLICK FOR MENU</h1>
+                        <p>LUNCH</p>
+                        <p>DINNER</p>
+                        <p>WINE LIST</p>
+                    </figure>
+                </div>
+                <section className="Globalcss GlobalComponent">
+                    <h1>CUISINE</h1>
+                    <p>Buona Terra serves contemporary Italian cuisine shaped by the personal vision of Chef Denis Lucchi. It starts with a ‘gourmet larder’ assembled from all the places in Italy where Chef Denis knows the best producers and their foodstuffs are located. These prized artisanal ingredients and the Italian recipes Chef Denis grew up with are the jump-off points for culinary artistry and adventure. Because of these, Chef Denis creates reinterpretations with more complex and intense flavours than the traditional dish, presenting them individually in contrast to the typically shared platters.</p>
+                    <p>There is a sense of challenge and pushing the envelope; of new discovery. Yet in the soul, Buona Terra is nothing but Italian – proudly, quintessentially, sublimely.</p>
+                    <h1 className="nobefore" >“Buon Appetito”</h1>
+                    <p>– From all of us at Buona Terra</p>
+                </section>
+                <div className="GlobalComponent1 Component2a" >
+                    <figure>
+                        <h1>LUNCH MENU</h1>
+                        <h2>BUONA TERRA <br /> SET MENU</h2>
+                        <p>4 COURSE &emsp; $118 <br /><br />
+                            2 GLASS WINE PAIRING &emsp;  $58 PER PERSON <br />
+                            3 GLASS WINE PAIRING &emsp;  $88 PER PERSON</p>
+                        <span>OPTION OF NATURAL WINE AVAILABLE</span>
+                        <h2 className="h22">BUONA TERRA <br />  LUNCH EXPERIENCE</h2>
+                        <p>5 COURSE &emsp; FROM $178 <br /> <br />
+                            LUNCH EXPERIENCE WINE PAIRING <br /> FROM $168 PER PERSON</p>
+                        <span>MENU WILL CHANGE BASED OF SEASONAL AND  AVAILABLE DAILY  INGREDIENTS, AND IS CONSIDERED FOR ALL TABLE</span>
+                        <button>VIEW LUNCH MENU</button>
+                        <span>Prices are subject to service charge & prevailing government taxes</span>
+                    </figure>
+                </div>
+                <div className="GlobalComponent1 Component3a" >
+                    <figure>
+                        <h1>DINNER MENU</h1>
+                        <h2>LEAVE IT TO CHEF LUCCHI   <br /> BUONA TERRA EXPERIENCE</h2>
+                        <p>5 COURSE ONWARDS &emsp; FROM $228</p>
+                        <span>MENU WILL CHANGE BASED OF SEASONAL AND AVAILABLE DAILY INGREDIENTS, AND IS CONSIDERED FOR ALL TABLE</span>
+                        <p>WINE PAIRING FROM $168 PER PERSON <br />
+                            <span>OPTION OF NATURAL WINE AVAILABLE</span>
+                            <br />
+                            WHISKY FLYER 1960/70/80/90 <br /> FROM $398 PER PERSON</p>
+                        <button>VIEW DINNER MENU</button>
+                        <span>Prices are subject to service charge & prevailing government taxes</span>
+                    </figure>
+                </div>
+                <section className="Globalcss GlobalComponent">
+                    <h1>WINE MENU</h1>
+                    <p>For the perfect gastronomic experience, let our sommelier recommend the perfect wine to pair
+                        your meal with from our wide selection.</p>
+                    <h1 className="nobefore" >“Buon Appetito”</h1>
+                    <p>– From all of us at Buona Terra</p>
+                </section>
+                <div className="GlobalComponent1 Component4a" >
+                    <figure>
+                        <h1>CARTA DEI VINI</h1>
+                        <p>Italy’s wines are among the most distinctive and best-loved in the world. Buona Terra boasts a collection of over 250 labels including vintages and limited bottles that are rarely available commercially, earning the restaurant recognition among the 5 top wine lists for Italian wines in Singapore.<br /><br />
+
+                            Buona Terra is one of the very few restaurants in Singapore to offer wine pairing for the entire menu, with the sommelier proposing ‘a la minute’ pairings based on the guest’s food selections.<br /><br />
+
+                            At Buona Terra, product passion combines with expert curation and cheerful sharing to make wine-drinking an act of sheer enjoyment.
+
+                        </p>
+                        <button>view wine menu</button>
+                    </figure>
+                </div>
+
+
+            </div>
+            <Footer />
         </>
     );
 }
